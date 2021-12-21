e0e3ae97b32492067c189dacac67c78b602b34bb
Pedro Porcelli <71854639+pedro-porcelli@users.noreply.github.com>
2021-12-21 19:29:18 -0300

    done with spam.js and spamming.html bugs.

--- spam.js
+++ spam.js
@@ -1,20 +1,28 @@
 personemail = getElementId('persons_email').value;
 youremail = getElementId('your_email').value;
 stop = getElementId('stop');
+stop_click = stop.addEventListener('click');
+
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "e727d816c64380",
+      pass: "73f2c56c5b4852"
+    }
+  });
+
+  message = {
+    from: youremail,
+    to: personemail,
+    subject: "Hey from" + youremail,
+    text: "Hi this is a spam from your freind at" + youremail
+}
 
 function spam() {
-    while (true) {
-        Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "galaxycraft.p@gmail.com",
-            Password : "6DC1B7B0ECDB1994C6F9B9714AFC9ADAC802",
-            To : personemail,
-            From : youremail,
-            Subject : "",
-            Body : "Hi i am spamming you"
-        })
-
-        if (stop.addEventListener('click')) {
+    while (true){
+        transporter.sendMail(message);
+        if (stop_click == true) {
             break;
         }
     }
